b74fea8a940396dcb63a8bc498061e3ef09cc380
Matt Flor <matthias.c.flor@gmail.com>
2015-05-20 19:55:50 +0200

    Follow RStudio advice of writing 0.1 instead of .1

--- inst/htmlwidgets/chorddiag.js
+++ inst/htmlwidgets/chorddiag.js
@@ -36,16 +36,16 @@ HTMLWidgets.widget({
     var groupnames = options.groupnames,
         groupcolors = options.groupcolors,
         tickInterval = options.tickInterval,
-        groupnamePadding = options.groupnamePadding
+        groupnamePadding = options.groupnamePadding;
 
     var chord = d3.layout.chord()
-        .padding(.05)
+        .padding(0.05)
         .sortSubgroups(d3.descending)
         .matrix(matrix);
 
     var width = 500,
         height = 500,
-        innerRadius = Math.min(width, height) * .31,
+        innerRadius = Math.min(width, height) * 0.31,
         outerRadius = innerRadius * 1.1;
 
     var fill = d3.scale.ordinal()
@@ -61,7 +61,7 @@ HTMLWidgets.widget({
         .style("fill", function(d) { return fill(d.index); })
         .style("stroke", function(d) { return fill(d.index); })
         .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
-        .on("mouseover", fade(.1))
+        .on("mouseover", fade(0.1))
         .on("mouseout", fade(1));
 
     var ticks = svg.append("g").attr("class", "ticks").selectAll("g")
@@ -95,7 +95,7 @@ HTMLWidgets.widget({
         .attr("d", d3.svg.chord().radius(innerRadius))
         .style("fill", function(d) { return fill(d.target.index); })
         .style("opacity", 1)
-        .on("mouseover", fade(.1))
+        .on("mouseover", fade(0.1))
         .on("mouseout", fade(1));
 
     // Returns an array of tick angles and labels, given a group.
@@ -150,7 +150,7 @@ HTMLWidgets.widget({
         .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180)translate(-50)" : null; })
         .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
         .text(function(d) { return d.label; })
-        .on("mouseover", fade2(.1))
+        .on("mouseover", fade2(0.1))
         .on("mouseout", fade2(1));
 
     function groupNames(d) {
