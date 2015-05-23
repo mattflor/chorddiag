6d2a413ee7b4a6e8b7c0bcc3d1894d9696ad449f
Matt Flor <matthias.c.flor@gmail.com>
2015-05-23 23:04:51 +0200

    Add package documentation file.

---
+++ R/chorddiag-package.R
@@ -0,0 +1,11 @@
+#' chorddiag: A package for creating interactive chord diagrams
+#'
+#' Create interactive chord diagrams using the JavaScript visualization library
+#' D3 (\url{http://d3js.org}). The chord diagram layout is based on
+#' \url{http://bl.ocks.org/mbostock/4062006}. Chord diagrams show directed
+#' relationships among a group of entities.
+#'
+#' @docType package
+#' @name chorddiag-package
+NULL
+#> NULL
